7880049c1ddd705b63b8aaf431442fc3ac0b3a67
russ.christinas <russ.christinas@gmail.com>
2022-10-07 12:59:26 -0500

    javalin basics

---
+++ Javalin/JavalinDemo/src/main/java/com/revature/Driver.java
@@ -0,0 +1,83 @@
+package com.revature;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.eclipse.jetty.http.HttpStatus;
+
+import com.revature.model.Person;
+
+import io.javalin.Javalin;
+import io.javalin.http.Context;
+
+public class Driver {
+	
+	public static void main(String[] args) {
+		
+		/*
+		 * Javalin is considered lightweight framework for quickly building RESTful APIs.
+		 * It comes with an embedded Jetty server and is an abstraction over an older
+		 * technology called servlets.
+		 */
+		
+		//Starts our server on port 8000
+		Javalin app = Javalin.create().start(8000);
+		
+		/*
+		 * As a matter of abstraction, sometimes we wish to perform a task before
+		 * an HTTP Request makes it to its destination. In order to avoid mixing this
+		 * logic into all of our endpoint logic, we can use an interceptor.
+		 */
+		
+		app.before(ctx -> {
+			System.out.println("This happens before the http requests make it to their final destination.");
+		});
+		
+		/*
+		 * Javalin uses lambda expressions heavily. It primarily uses the Handler
+		 * interface. This interface has a single abstract method called handle that
+		 * takes a Context object and has a void return type.
+		 * 
+		 * As a note, the Context is an abstraction over servlet internals that allow
+		 * us to access and manipulate the HTTP request and HTTP response as Java objects.
+		 */
+		
+		app.get("/person/{id}", (Context ctx) -> {
+			/*
+			 * You can also take a Java object and serialize it as JSON before writing it
+			 * to the response body.
+			 */
+			Set<Person> people = new HashSet<>();
+			Person person = new Person(1, "Sean", 23);
+			Person person2 = new Person(2, "Zeke", 22);
+			Person person3 = new Person(3, "Jen", 25);
+			
+			people.add(person);
+			people.add(person2);
+			people.add(person3);
+			
+			Person selectedPerson = null;
+			
+			for(Person p : people) {
+				if(p.getId() == Integer.parseInt(ctx.pathParam("id"))) selectedPerson = p;
+			}
+			
+			ctx.json(selectedPerson);
+		});
+		
+		app.post("/new-item", ctx -> {
+			/*
+			 * If a client is making a POST request, this means that there must be something
+			 * in the HTTP request body that we can extract.
+			 * 
+			 * Note that we can extract the body as a String, or we can immediately marshal
+			 * the body into a Java object.
+			 */
+			Person receivedPerson = ctx.bodyAsClass(Person.class);
+			System.out.println(receivedPerson);
+			ctx.status(HttpStatus.CREATED_201);
+			
+		});
+	}
+
+}
---
+++ Javalin/JavalinDemo/src/main/java/com/revature/model/Person.java
@@ -0,0 +1,91 @@
+package com.revature.model;
+
+/*
+ * I typically create my Java models as "Java Beans". A Java Bean typically includes:
+ * 
+ * 1) private fields
+ * 2) a no-args constructor
+ * 3) a constructor using all of the fields
+ * 4) getters and setters
+ * 5) a hashCode and equals method
+ * 6) a toString method
+ * 7) Technically, a Java Bean should implement the Serializable interface, but this is often ignored.
+ */
+public class Person {
+
+	private int id;
+	private String name;
+	private int age;
+
+	public Person() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Person(int id, String name, int age) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.age = age;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + age;
+		result = prime * result + id;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Person other = (Person) obj;
+		if (age != other.age)
+			return false;
+		if (id != other.id)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Person [id=" + id + ", name=" + name + ", age=" + age + "]";
+	}
+}
