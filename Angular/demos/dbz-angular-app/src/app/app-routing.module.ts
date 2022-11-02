ebe398381379f05df96b00f1ec324872b6bc0ad1
aaknox <azhya.knox@gmail.com>
2022-11-02 13:08:07 -0500

    added demo component

--- Angular/demos/dbz-angular-app/src/app/app-routing.module.ts
+++ Angular/demos/dbz-angular-app/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
-const routes: Routes = [];
+//For page navigation, Angular has a special module dedicated for this task known as the app-routing.module.ts file
+//here we will define our routes (aka our URL paths to specific pages in our Angular) by declaring the path and its component in the Routes array below
+const routes: Routes = [
+  {
+    //* = wildcard for routes
+    path: "404", 
+    component: NotFoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
